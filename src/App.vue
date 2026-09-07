9d614ee6771ae7140d7e4d2a1b90ebdf50240e40
Mahathir Arch <mhmmdmhthr@gmail.com>
2026-09-07 08:11:02 +0700

    feat: add html shell and public assets

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { Toaster } from 'vue-sonner'
+
+import ThemeProvider from '@/components/theme-provider.vue'
+import { useAuthStore } from '@/stores/auth'
+
+const auth = useAuthStore()
+
+onMounted(() => auth.init())
+</script>
+
+<template>
+  <ThemeProvider default-theme="light" storage-key="hadirin-theme">
+    <div class="font-sans antialiased" style="font-family: var(--font-inter)">
+      <RouterView />
+      <Toaster rich-colors position="top-right" />
+    </div>
+  </ThemeProvider>
+</template>
